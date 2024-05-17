97235e4d272290d8a95d11a17fa90e916072094e
smarttarded <khiemgluong@gmail.com>
2024-05-17 09:38:10 -0700

    initial commit

---
+++ lib/jekyll/obsidian.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "obsidian/version"
+
+module Jekyll
+  module Obsidian
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
