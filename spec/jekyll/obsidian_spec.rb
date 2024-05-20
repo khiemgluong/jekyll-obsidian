2b6b5eec1b85b7004cea181cf20429e7c5fc9f43
smarttarded <khiemgluong@gmail.com>
2024-05-19 18:17:47 -0700

    test 3

--- spec/jekyll/obsidian_spec.rb
+++ spec/jekyll/obsidian_spec.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 require "spec_helper"
+require "tmpdir"
 
 RSpec.describe Jekyll::Obsidian do
   it "has a version number" do
     expect(Jekyll::Obsidian::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # Commented out for now as it's designed to fail
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
 end
+
 # spec/jekyll/obsidian_spec.rb
 
 require "spec_helper"
+require "tmpdir"
 
 RSpec.describe Jekyll::Obsidian::FileTreeGenerator do
   it "generates a file tree" do
-    site = double("site")
-    allow(site).to receive(:config).and_return({ "file_tree_source" => "source_dir" })
-    allow(site).to receive(:source).and_return("source_dir")
-    allow(site).to receive(:data).and_return({})
+    Dir.mktmpdir do |dir|
+      site = double("site")
+      allow(site).to receive(:config).and_return({ "file_tree_source" => dir })
+      allow(site).to receive(:source).and_return(dir)
+      allow(site).to receive(:data).and_return({})
 
-    generator = Jekyll::Obsidian::FileTreeGenerator.new
-    expect { generator.generate(site) }.not_to raise_error
+      generator = Jekyll::Obsidian::FileTreeGenerator.new
+      expect { generator.generate(site) }.not_to raise_error
+    end
   end
 end
