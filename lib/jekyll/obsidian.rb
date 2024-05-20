41798a648bdb3c4d720b2c0cb50b181315c57329
smarttarded <khiemgluong@gmail.com>
2024-05-20 03:38:54 -0700

    feat: Copy file_tree.html to _obsidian directory

--- lib/jekyll/obsidian.rb
+++ lib/jekyll/obsidian.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 require "jekyll"
+require "json"
 
 require_relative "obsidian/version"
 
@@ -14,15 +15,8 @@ module Jekyll
       def generate(site)
         source_dir = site.config["file_tree_source"] || site.source
         file_tree = generate_file_tree(source_dir)
-
         site.data["file_tree"] = file_tree
         obsidian_dir = File.join(File.dirname(site.dest), "_obsidian")
-        # puts site.dest
-        # project_root = File.expand_path("../..", File.dirname(__FILE__))
-        # assets_dir = File.join(project_root, "assets")
-        # FileUtils.mkdir_p(obsidian_dir) unless File.directory?(obsidian_dir)
-        # FileUtils.cp(File.join(assets_dir, "file_tree.js"), obsidian_dir)
-        # FileUtils.cp(File.join(assets_dir, "file_tree.css"), obsidian_dir)
 
         project_root = File.expand_path("../..", File.dirname(__FILE__))
         assets_dir = File.join(project_root, "assets")
@@ -33,6 +27,15 @@ module Jekyll
         file_tree_js = File.join(assets_dir, "js", "file_tree.js")
         puts file_tree_js
         file_tree_css = File.join(assets_dir, "css", "file_tree.css")
+        file_tree_html = File.join(assets_dir, "widgets", "file_tree.html")
+
+        if File.exist?(file_tree_html)
+          result = FileUtils.cp(file_tree_html, obsidian_dir)
+          puts "Copy result for file_tree.html: #{result}"
+        else
+          puts "Error: #{file_tree_html} does not exist"
+          exit
+        end
 
         if File.exist?(file_tree_js)
           result = FileUtils.cp(file_tree_js, obsidian_dir)
@@ -58,10 +61,11 @@ module Jekyll
         Dir.entries(dir).each do |entry|
           next if entry == "." || entry == ".." || entry.start_with?("_")
           path = File.join(dir, entry)
+          puts "file path: #{path}"  # print the path
           tree << if File.directory?(path)
-            {name: entry, type: "directory", children: generate_file_tree(path)}
+            { name: entry, type: "dir", children: generate_file_tree(path) }
           else
-            {name: entry, type: "file"}
+            { name: entry, type: "file" }
           end
         end
         tree
